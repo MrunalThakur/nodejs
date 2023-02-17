ff7fe71ed807905dadd17bdfd5ee6a707bd74335
MSThakur <ThakurM@versifit.com>
2023-02-17 11:39:30 +0530

    writing response to file

--- training/server.js
+++ training/server.js
@@ -31,22 +31,28 @@ const server = http.createServer((req,res) => {
         res.setHeader('Content-Type','text/html');
         res.write('<html>');
         res.write('<head><title> This is so weird</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="AtextBoxData"><button>Submit</button></form></body>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="textBoxData"><button>Submit</button></form></body>');
         res.write('</html>');
         return res.end();
 
     }
     if(url === '/message' && method === 'POST'){
         const body = [];
-        req.on('data',(dataChunk) => {
+        //on method is creates a listener
+        //'data' is the event we're listening to
+        req.on('data',(dataChunk) => { 
             console.log(dataChunk);
             body.push(dataChunk);
         }); 
+        //'end' listener fires when all incoming data is parsed
         req.on('end',()=>{
+            //Buffer object is available globally (nodejs feature)
             const parseBody = Buffer.concat(body).toString();
-            console.log(parseBody);
+            //console.log(parseBody);
+            const txtMessage = parseBody.split('=')[1];
+            fs.writeFileSync('responseFile.text',txtMessage);
+
         });
-        fs.writeFileSync('responseFile.text','Dummy Data');
         //res.writeHead(302,{}) //{} has headers
         //easier to read if written on separate lines
         res.statusCode = 302; //302 - redirect
@@ -63,7 +69,7 @@ const server = http.createServer((req,res) => {
     res.end(); //cannot write res.write after res.end
 });
 
-server.listen(1433); //1433 port
+server.listen(3000); //1433 port
 
 //better way of writing response is using the Express.js framework
 //this is just to understand the internal working
