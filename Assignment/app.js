9c0f95835b4ab7fa2b0fbbee94da9bc1e7632f76
msthakur <msthakur.signups@gmail.com>
2023-03-14 16:30:34 +0530

    delete and display by id completed

--- Assignment/app.js
+++ Assignment/app.js
@@ -41,7 +41,8 @@ app.post('/myapp/user/add',(req,res)=>{
 
 app.put('/myapp/user/update/:id',(req,res)=>{
     // console.log('File exists');
-    const id = req.params.id.split(':')[1].toString().replace(/["]+/g,'');
+    //const id = req.params.id.split(':')[1].toString().replace(/["]+/g,'');
+    const id = req.params.id; 
     let index = userData.user.findIndex((item)=>{return item.id == id});
     // console.log("index"+index);
     if(index > -1){
@@ -68,6 +69,33 @@ app.get('/myapp/user/display',(req,res)=>{
     }
     console.log("Response Sent");
 
+});
+
+app.get('/myapp/user/display/:id',(req,res)=>{
+    const id = req.params.id;
+    let index = userData.user.findIndex((item) => { return item.id === id});
+    res.write('<html>');
+    res.write('<strong>ID:</strong> ' + userData.user[index].id + ', <strong>Name:</strong> ' + userData.user[index].name )
+    res.write('</html>');
+});
+//to delete a user by id from json file using expressjs
+
+app.delete('/myapp/user/delete/:id',(req,res)=>{
+    const id = req.params.id;
+    let index = userData.user.findIndex((item)=>{ return item.id === id});
+    if(index >-1){
+
+        //removes the user from array
+        userData.user.splice(index,1);
+        //update the file
+        let data = JSON.stringify(userData,null,2);
+        fs.writeFileSync('userData.json',data);
+        console.log("user deleted");
+        res.status(200).send({message: "User deleted"});
+    }
+    else{
+        res.status(404).send({message: "User not found"});
+    }
 });
 
 //html page to display user id and name
